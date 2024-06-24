a27feff80199a6866f2b7218b014c0bdd81cc30d
Sujal <sujalkrish007@gmail.com>
2024-06-24 13:56:40 +0530

    Portfolio image grid changed swiping gallery,more button bug fix,Added title for the images and videos

--- script.js
+++ script.js
@@ -47,7 +47,28 @@ const displayList = () => {
 
 //circle-bg-animations
 
-  
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.querySelectorAll(".mySlides");
+  var dots = document.querySelectorAll(".dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
 btnHamburger.addEventListener('click', displayList)
 
